7cd1776dd38f591e5bf113969a000a86d1b6a11b
github username <github email address>
2020-04-10 23:34:06 -0700

    Hand class in progress

--- app/hand.py
+++ app/hand.py
@@ -3,4 +3,24 @@ class Hand():
         self.cards = cards
 
     def best_rank(self):
+        # card_rank_counts = {}
+        # for card in self.cards:
+        #     card_rank_counts.setdefault(card.rank, 0)
+        #     card_rank_counts[card.rank] += 1
+
+        ranks_with_pairs = {
+            rank: rank_count
+            for rank, rank_count in self.card_rank_counts.items()
+            if rank_count == 2
+        }
+
+        for rank_count in card_rank_counts.values():
+            if rank_count == 2:
+                return 'Pair'
+
         return 'High Card'
+
+
+@property
+def _card_rank_counts(self):
+    card_rank_counts = {}
