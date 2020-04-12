4d5fe878269e1a3a445eca673e1c1159f05e8902
github username <github email address>
2020-04-11 17:03:13 -0700

    Hand class in progress

--- app/hand.py
+++ app/hand.py
@@ -3,24 +3,24 @@ class Hand():
         self.cards = cards
 
     def best_rank(self):
-        # card_rank_counts = {}
-        # for card in self.cards:
-        #     card_rank_counts.setdefault(card.rank, 0)
-        #     card_rank_counts[card.rank] += 1
-
         ranks_with_pairs = {
             rank: rank_count
-            for rank, rank_count in self.card_rank_counts.items()
+            for rank, rank_count in self._card_rank_counts.items()
             if rank_count == 2
         }
 
-        for rank_count in card_rank_counts.values():
-            if rank_count == 2:
-                return 'Pair'
+        if len(ranks_with_pairs) == 2:
+            return 'Two Pair'
+
+        if len(ranks_with_pairs) == 1:
+            return 'Pair'
 
         return 'High Card'
 
-
-@property
-def _card_rank_counts(self):
-    card_rank_counts = {}
+    @property
+    def _card_rank_counts(self):
+        card_rank_counts = {}
+        for card in self.cards:
+            card_rank_counts.setdefault(card.rank, 0)
+            card_rank_counts[card.rank] += 1
+        return card_rank_counts
--- test/test_hand.py
+++ test/test_hand.py
@@ -22,14 +22,38 @@ class TestHand:
         hand = Hand(cards=cards)
         assert hand.best_rank() == 'High Card'
 
+    def test_hand_figures_out_single_pair_is_best_rank(self):
+        cards = [
+            Card(rank='Jack', suit='Spades'),
+            Card(rank='4', suit='Clubs'),
+            Card(rank='7', suit='Diamonds'),
+            Card(rank='King', suit='Diamonds'),
+            Card(rank='7', suit='Hearts')
+        ]
+
+        hand = Hand(cards=cards)
+        assert hand.best_rank() == 'Pair'
+
     def test_hand_figures_out_two_pair_is_best_rank(self):
         cards = [
             Card(rank='Ace', suit='Spades'),
             Card(rank='9', suit='Clubs'),
             Card(rank='7', suit='Diamonds'),
-            Card(rank='King', suit='Spades'),
+            Card(rank='Ace', suit='Diamonds'),
             Card(rank='7', suit='Hearts')
         ]
 
         hand = Hand(cards=cards)
         assert hand.best_rank() == 'Two Pair'
+
+    # def test_hand_figures_out_three_of_a_kind_is_best_rank(self):
+    #     cards = [
+    #         Card(rank='Ace', suit='Spades'),
+    #         Card(rank='9', suit='Clubs'),
+    #         Card(rank='7', suit='Diamonds'),
+    #         Card(rank='Ace', suit='Diamonds'),
+    #         Card(rank='7', suit='Hearts')
+    #     ]
+
+    #     hand = Hand(cards=cards)
+    #     assert hand.best_rank() == 'Three of a kind'
